1b58521f635e851774055169cf944808657e5def
sahil1815 <152404082+sahil1815@users.noreply.github.com>
2025-10-29 22:25:59 +0600

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Tilt } from "react-tilt";
 import ReactTypingEffect from "react-typing-effect";
 import profileImage from "../../assets/profile2.png";
 
@@ -85,15 +84,15 @@ const About = () => {
               <div className="h-3 w-3 rounded-full bg-orange-400"></div>
               <div className="h-3 w-3 rounded-full bg-green-400"></div>
             </div>
-            <div className="text-xs text-gray-500 text-gray-400 font-mono">
+            <div className="text-xs text-gray-500 font-mono">
               coder.js
             </div>
           </div>
-          <div className="overflow-hidden border-t-[2px] border-gray-300 border-indigo-900 px-4 lg:px-8 py-4 lg:py-8 relative">
+          <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8 relative">
             <div className="absolute -top-24 -left-24 w-56 h-56 bg-blue-600 rounded-full opacity-5 opacity-10 filter blur-3xl"></div>
             <div className="absolute -bottom-24 -right-24 w-56 h-56 bg-pink-600 rounded-full opacity-5 opacity-10 filter blur-3xl"></div>
             <div className="relative flex">
-              <div className="hidden md:flex flex-col items-end pr-4 text-gray-400 text-gray-500 text-xs">
+              <div className="hidden md:flex flex-col items-end pr-4 text-gray-500 text-xs">
                 <div className="leading-relaxed select-none opacity-70 font-mono">1</div>
                 <div className="leading-relaxed select-none opacity-70 font-mono">2</div>
                 <div className="leading-relaxed select-none opacity-70 font-mono">3</div>
@@ -115,119 +114,119 @@ const About = () => {
                   <span className="mr-2 text-blue-600 font-mono text-violet-400">
                     coder
                   </span>
-                  <span className="mr-2 text-purple-700 font-mono text-pink-400">=</span>
-                  <span className="text-gray-500 text-gray-400 font-mono">{"{"}</span>
+                  <span className="mr-2 font-mono text-pink-400">=</span>
+                  <span className="text-gray-500 font-mono">{"{"}</span>
                 </div>
                 <div className="pl-6">
-                  <span className="text-gray-800 text-white font-mono">name: </span>
-                  <span className="text-gray-500 text-gray-400 font-mono">'</span>
-                  <span className="text-emerald-600 text-green-400 font-mono">
+                  <span className="text-white font-mono">name: </span>
+                  <span className="text-gray-500 font-mono">'</span>
+                  <span className="text-green-400 font-mono">
                     Sanzid Sahil
                   </span>
-                  <span className="text-gray-500 font-mono text-gray-400">',</span>
+                  <span className="text-gray-500 font-mono">',</span>
                 </div>
                 <div className="pl-6">
-                  <span className="text-gray-800 font-mono text-white">role: </span>
-                  <span className="text-gray-500 font-mono text-gray-400">'</span>
-                  <span className="text-emerald-600 font-mono text-green-400">
+                  <span className="font-mono text-white">role: </span>
+                  <span className="text-gray-500 font-mono">'</span>
+                  <span className="font-mono text-green-400">
                     Frontend Developer
                   </span>
-                  <span className="text-gray-500 font-mono text-gray-400">',</span>
+                  <span className="text-gray-500 font-mono">',</span>
                 </div>
                 <div className="pl-6">
-                  <span className="text-gray-800 font-mono text-white">seniority: </span>
-                  <span className="text-gray-500 font-mono text-gray-400">'</span>
-                  <span className="text-emerald-600 font-mono text-green-400">
+                  <span className="font-mono text-white">seniority: </span>
+                  <span className="text-gray-500 font-mono">'</span>
+                  <span className="font-mono text-green-400">
                     Junior
                   </span>
-                  <span className="text-gray-500 font-mono text-gray-400">',</span>
+                  <span className="text-gray-500 font-mono">',</span>
                 </div>
                 <div className="pl-6">
-                  <span className="text-gray-800 font-mono text-white">location: </span>
-                  <span className="text-gray-500 font-mono text-gray-400">'</span>
-                  <span className="text-emerald-600 font-mono text-green-400">
+                  <span className="font-mono text-white">location: </span>
+                  <span className="text-gray-500 font-mono">'</span>
+                  <span className="font-mono text-green-400">
                     Bangladesh
                   </span>
-                  <span className="text-gray-500 font-mono text-gray-400">',</span>
+                  <span className="text-gray-500 font-mono">',</span>
                 </div>
                 <div className="pl-6">
-                  <span className="text-gray-800 font-mono text-white">email: </span>
-                  <span className="text-gray-500 font-mono text-gray-400">'</span>
-                  <span className="text-emerald-600 font-mono text-green-400">
+                  <span className="font-mono text-white">email: </span>
+                  <span className="text-gray-500 font-mono">'</span>
+                  <span className="font-mono text-green-400">
                     sanzid.sahil.999@gmail.com 
                   </span>
-                  <span className="text-gray-500 font-mono text-gray-400">',</span>
+                  <span className="text-gray-500 font-mono">',</span>
                 </div>
                 <div className="pl-6">
-                  <span className="text-gray-800 font-mono text-white">skills: </span>
-                  <span className="text-gray-500 font-mono text-gray-400">[</span>
+                  <span className="font-mono text-white">skills: </span>
+                  <span className="text-gray-500 font-mono">[</span>
                   <div className="pl-6 flex font-mono flex-wrap">
                     <span className="tech-item font-mono">
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                       <span className="text-cyan-400 font-mono">React</span>
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
-                      <span className="text-gray-500 font-mono text-gray-400 ">, </span>
+                      <span className="text-gray-500 font-mono">'</span>
+                      <span className="text-gray-500 font-mono ">, </span>
                     </span>
                     <span className="tech-item">
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                       <span className="text-cyan-400 font-mono">TailwindCSS</span>
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
-                      <span className="text-gray-500 font-mono text-gray-400 ">, </span>
+                      <span className="text-gray-500 font-mono">'</span>
+                      <span className="text-gray-500 font-mono ">, </span>
                     </span>
                     <span className="tech-item">
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                       <span className="text-cyan-400 font-mono">NodeJS</span>
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
-                      <span className="text-gray-500 font-mono text-gray-400 ">,</span>
+                      <span className="text-gray-500 font-mono">'</span>
+                      <span className="text-gray-500 font-mono ">,</span>
                     </span>
                     <span className="tech-item">
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                       <span className="text-cyan-400 font-mono">CSS</span>
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
-                      <span className="text-gray-500 font-mono text-gray-400 ">, </span>
+                      <span className="text-gray-500 font-mono">'</span>
+                      <span className="text-gray-500 font-mono ">, </span>
                     </span>
                     <span className="tech-item">
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                       <span className="text-cyan-400 font-mono">HTML</span>
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
-                      <span className="text-gray-500 font-mono text-gray-400 ">, </span>
+                      <span className="text-gray-500 font-mono">'</span>
+                      <span className="text-gray-500 font-mono ">, </span>
                     </span>
                     <span className="tech-item">
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                       <span className="text-cyan-400 font-mono">Figma</span>
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
-                      <span className="text-gray-500 font-mono text-gray-400 ">, </span>
+                      <span className="text-gray-500 font-mono">'</span>
+                      <span className="text-gray-500 font-mono ">, </span>
                     </span>
                     <span className="tech-item">
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                       <span className="text-cyan-400 font-mono">Git</span>
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
-                      <span className="text-gray-500 font-mono text-gray-400 ">, </span>
+                      <span className="text-gray-500 font-mono">'</span>
+                      <span className="text-gray-500 font-mono ">, </span>
                     </span>
                     <span className="tech-item">
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                       <span className="text-cyan-400 font-mono">GitHub</span>
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
-                      <span className="text-gray-500 font-mono text-gray-400 ">, </span>
+                      <span className="text-gray-500 font-mono">'</span>
+                      <span className="text-gray-500 font-mono ">, </span>
                     </span>
                     <span className="tech-item">
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                       <span className="text-cyan-400 font-mono">JavaScript</span>
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
-                      <span className="text-gray-500 font-mono text-gray-400 ">, </span>
+                      <span className="text-gray-500 font-mono">'</span>
+                      <span className="text-gray-500 font-mono ">, </span>
                     </span>
                     <span className="tech-item">
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                       <span className="text-cyan-400 font-mono">TypeScript</span>
-                      <span className="text-gray-500 font-mono text-gray-400">'</span>
+                      <span className="text-gray-500 font-mono">'</span>
                     </span>
                   </div>
                   <div className="pl-0">
-                    <span className="text-gray-500 font-mono text-gray-400">],</span>
+                    <span className="text-gray-500 font-mono">],</span>
                   </div>
                 </div>
                 <div>
-                  <span className="text-gray-500 text-gray-400 font-mono">{"};"}</span>
+                  <span className="text-gray-500 font-mono">{"};"}</span>
                 </div>
               </code>
             </div>
